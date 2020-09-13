d4b0253311bebae3ca3def8429204061e1cc28a3
Viktar Belski <belski.viktor.dev@gmail.com>
2020-09-13 23:56:19 +0300

    feat: cover component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,11 +7,13 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { LoginComponent } from './components/login';
+import { CoverComponent } from './components/cover';
 
 @NgModule({
   declarations: [
     RegisterComponent,
-    LoginComponent
+    LoginComponent,
+    CoverComponent
   ],
   imports: [
     CommonModule,
@@ -26,7 +28,8 @@ import { LoginComponent } from './components/login';
   ],
   exports: [
     RegisterComponent,
-    LoginComponent
+    LoginComponent,
+    CoverComponent
   ]
 })
 
