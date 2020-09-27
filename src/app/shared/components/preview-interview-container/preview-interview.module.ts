848c71a09a6c52d886041eb8f51ba0892d8fc568
Viktar Belski <belski.viktor.dev@gmail.com>
2020-09-27 22:47:20 +0300

    feat: control-panel styles

--- src/app/shared/components/preview-interview-container/preview-interview.module.ts
+++ src/app/shared/components/preview-interview-container/preview-interview.module.ts
@@ -3,6 +3,8 @@ import { PreviewInterviewContainerComponent } from './preview-interview-containe
 import { PreviewInterviewComponent } from './previrew-interview';
 import { CommonModule } from '@angular/common';
 import { ControlPanelComponent } from './control-panel';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -11,7 +13,9 @@ import { ControlPanelComponent } from './control-panel';
     ControlPanelComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule,
+    MatButtonModule
   ],
   exports: [PreviewInterviewContainerComponent]
 })
