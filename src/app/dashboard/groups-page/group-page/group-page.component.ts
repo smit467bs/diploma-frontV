343bbc1df48f6337d98126c0f8074318eaeccd48
Viktar Belski <belski.viktor.dev@gmail.com>
2020-12-26 15:46:26 +0300

    feat: access to group page

--- src/app/dashboard/groups-page/group-page/group-page.component.ts
+++ src/app/dashboard/groups-page/group-page/group-page.component.ts
@@ -1,11 +1,10 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
-import { catchError, first, switchMap, tap } from 'rxjs/operators';
+import { switchMap } from 'rxjs/operators';
 
 import { getRouteParam$ } from 'core/utils';
-import { GroupService, UsersService } from 'core/services';
-import { UserInfo } from 'core/store/user/models';
+import { GroupService } from 'core/services';
 
 @Component({
   selector: 'app-group-page',
@@ -13,38 +12,27 @@ import { UserInfo } from 'core/store/user/models';
   styleUrls: ['./group-page.component.scss']
 })
 export class GroupPageComponent implements OnInit {
-  @ViewChild('invite') inviteField: ElementRef;
   id$: Observable<string>;
 
+  loading: boolean = true;
+  errorMessage: string = null;
   group: any;
-  invitedUsers: Array<UserInfo> = [];
 
   constructor(private activatedRoute: ActivatedRoute,
-              private groupService: GroupService,
-              private usersService: UsersService) {
+              private groupService: GroupService) {
     this.id$ = getRouteParam$(this.activatedRoute, 'id');
   }
 
   ngOnInit() {
     this.id$.pipe(
       switchMap(id => this.groupService.getGroupById(id)),
-      tap(group => this.group = group)
-    ).subscribe();
-  }
-
-  inviteUser(email: string) {
-    console.log(email);
-    this.usersService.findUserByEmail(email).pipe(
-      first(),
-      tap((userInfo) => {
-        this.invitedUsers.push(userInfo);
-        this.inviteField.nativeElement.value = '';
-      }),
-      catchError(err => {
-        console.log(email);
-        return err;
-      })
-    ).subscribe();
+    ).subscribe(group => {
+      this.group = group;
+      this.loading = false;
+    }, ({error}) => {
+      this.errorMessage = error.error.message;
+      this.loading = false;
+    });
   }
 
 }
